784745d6af926bee23c8ae3cf0cad8d7b31fbc18
Lucas <lucas.wychodil-lubi@fh-joanneum.at>
2025-11-16 13:12:05 +0100

    created css draft

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,6 @@ export default function Home() {
         <div>
           <h1>
             <br />
-            <br />
-            <br />
             Willkommen bei dem
             <br />
             Raumbuchungssystem der
