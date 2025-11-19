355285965d2f998ac16c1222297195b161bc7f11
Alia Alamer <alia.alamer@edu.fh-joanneum.at>
2025-11-19 14:12:09 +0100

    added user table (pair programming with Linda)

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,17 @@ CREATE TABLE role (
     role_id INT AUTO_INCREMENT PRIMARY KEY,
     role_name VARCHAR(50) NOT NULL
 )
+
+CREATE TABLE user (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    role_id INT NOT NULL,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100) Not NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    account_deactivated BOOLEAN DEFAULT NULL, -- NULL = aktiv, 1= deaktiviert
+    first_login BOOLEAN DEFAULT 0 NOT NULL, -- 0= muss Passwort ändern, 1= normal login
+    FOREIGN KEY (role_id) REFERENCES role(role_id)
+)
+
