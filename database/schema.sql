b6798fe7d9d50f3b27a3b6f37e8dde8f917e6b8b
Alia Alamer <alia.alamer@edu.fh-joanneum.at>
2025-11-19 13:34:10 +0100

    started with the sql script and added first table (pair programming with Linda)

---
+++ database/schema.sql
@@ -0,0 +1,7 @@
+-- CREATE DATABASE IF NOT EXISTS room_booking_system;
+-- USE room_booking_system;
+
+CREATE TABLE role (
+    role_id INT AUTO_INCREMENT PRIMARY KEY,
+    role_name VARCHAR(50) NOT NULL
+)
