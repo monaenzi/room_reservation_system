2c4f0c3877d7450c6d54e14a35d73e8cf9021703
Lucas <lucas.wychodil-lubi@fh-joanneum.at>
2025-11-20 16:26:51 +0100

    added role to query

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -36,7 +36,7 @@ export async function POST(req: NextRequest) {
 
   
     const rows = await conn.query(
-      "SELECT user_id, email, password_hash, first_login, account_deactivated FROM users WHERE email = ? LIMIT 1",
+      "SELECT user_id, email, password_hash, first_login, account_deactivated, role_id FROM users WHERE email = ? LIMIT 1",
       [email]
     );
 
