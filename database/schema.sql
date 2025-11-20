9a7cc98b6a42d0410ef1e3d84d83ccd090238e29
Alia Alamer <alia.alamer@edu.fh-joanneum.at>
2025-11-20 10:25:06 +0100

    correct some mistakes in the sql script

--- database/schema.sql
+++ database/schema.sql
@@ -1,21 +1,18 @@
--- CREATE DATABASE IF NOT EXISTS room_booking_system;
--- USE room_booking_system;
-
 CREATE TABLE role (
     role_id INT AUTO_INCREMENT PRIMARY KEY,
     role_name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE user (
+CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     role_id INT NOT NULL,
     username VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     first_name VARCHAR(100) Not NULL,
     last_name VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    account_deactivated BOOLEAN DEFAULT 0 NOT NULL, -- 0 = aktiv, 1= deaktiviert
-    first_login BOOLEAN DEFAULT 0 NOT NULL, -- 0= muss Passwort ändern, 1= normal login
+    account_deactivated TINYINT(1) DEFAULT 0 NOT NULL, -- 0 = aktiv, 1= deaktiviert
+    first_login TINYINT(1) DEFAULT 0 NOT NULL, -- 0= muss Passwort ändern, 1= normal login
     FOREIGN KEY (role_id) REFERENCES role(role_id)
 );
 
@@ -26,25 +23,25 @@ CREATE TABLE room (
     room_capacity INT,
     floor_number INT,
     building VARCHAR(100),
-    is_visible BOOLEAN DEFAULT TRUE NOT NULL,
+    is_visible TINYINT(1) DEFAULT 0 NOT NULL,
     created_by INT,
-    FOREIGN KEY (created_by) REFERENCES user(user_id)
+    FOREIGN KEY (created_by) REFERENCES users(user_id)
 );
 
 CREATE TABLE timeslot (
-    timeslot_id INT PRIMARY KEY AUTO_INCREMENT,
+    timeslot_id INT AUTO_INCREMENT PRIMARY KEY,
     room_id INT NOT NULL,
     timeslot_status INT DEFAULT 0 NOT NULL, -- status: 0 = not released/inactive, 1 = available, 2 = reserved, 3 = blocked by admin
     slot_date DATE NOT NULL,
     start_time TIME NOT NULL,
     end_time TIME NOT NULL,
     blocked_reason TEXT,
-    name VARCHAR(100),
-    FOREIGN KEY (room_id) REFERENCES Room(room_id)
+    timeslot_name VARCHAR(100),
+    FOREIGN KEY (room_id) REFERENCES room(room_id)
 );
  
 CREATE TABLE booking (
-    booking_id INT PRIMARY KEY AUTO_INCREMENT,
+    booking_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     timeslot_id INT NOT NULL,
     reason TEXT NOT NULL,
@@ -52,6 +49,6 @@ CREATE TABLE booking (
     requested_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     responded_at TIMESTAMP NULL,
     cancelled_at TIMESTAMP NULL,
-    FOREIGN KEY (user_id) REFERENCES User(user_id),
-    FOREIGN KEY (timeslot_id) REFERENCES Timeslot(timeslot_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (timeslot_id) REFERENCES timeslot(timeslot_id)
 );
