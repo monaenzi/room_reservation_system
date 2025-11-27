e65b8448adfe68b812c1aba9b90f2e9bfa9f6631
Alia Alamer <alia.alamer@edu.fh-joanneum.at>
2025-11-27 20:29:17 +0100

    added request parsing and validation parse room_name, room_description, room_capacity, floor_number, building, created_by validate that room_name and that created_by are provided
    also added admin permission verification

--- app/api/rooms/route.ts
+++ app/api/rooms/route.ts
@@ -14,8 +14,65 @@ export async function POST(req:NextRequest) {
     let conn: mariadb.PoolConnection | undefined;
 
     try{
+
+        const {
+            room_name,
+            room_description,
+            room_capacity,
+            floor_number,
+            building,
+            created_by
+        } = await req.json();
+
+        if (!room_name){
+            return NextResponse.json(
+                {message: "Raumname ist erforderlich"},
+                {status: 400}
+            );
+        }
+
+        if(!created_by){
+            return NextResponse.json(
+                {message: "Admin Id ist erforderlich"},
+                {status: 400}
+            );
+        }
+
+
+        try {
+            conn = await pool.getConnection();
+        } catch (err) {
+            console.error("db Verbindung fehlgeschlagen:", err);
+            return NextResponse.json (
+                {message : "Verbindung zur DB nicht möglich"},
+                {status: 500}
+            );
+        }
+
+        const adminCheck = await conn.query(
+            "SELECT user_id, role_id FROM users WHERE  user_id = ? LIMIT 1",
+            [created_by]
+        );
+
+        if(!adminCheck || adminCheck.length === 0){
+            return NextResponse.json(
+                {message: "Benutzer nicht gefunden"},
+                {status: 404}
+            );
+        }
+
+        if(adminCheck[0].role_id !== 1){
+            return NextResponse.json(
+                {message: "Keine Berechtigung. Nur Administratoren können Räume erstellen"},
+                {status: 403}
+            );
+        }
+
+
+        
+
         return NextResponse.json(
-            {message: "room creation endpoint"},
+            {message: "validation passed"},
             {status: 200}
         );
     } catch (err) {
